3f528aaad553c5d238ef74278f9326d53480aa00
ntdang <ndang83@gmail.com>
2018-02-15 21:34:38 -0500

    Added reset function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,33 @@
 $('.document').ready(function() {
 
-  //Create variables for random number, wins, losses, total score
+  //Create variables for random number, wins, losses, total score, crystal values
+  var randomNumber = Math.floor(Math.random() * 120) + 19;
+    console.log(randomNumber);
+  var wins = 0;
+  var losses = 0;
+  var totalScore = 0;
+  var greenGem = Math.floor(Math.random() * 12) + 1;
+    console.log(greenGem);
+  var multiGem = Math.floor(Math.random() * 12) + 1;
+    console.log(multiGem);
+  var silverGem = Math.floor(Math.random() * 12) + 1;
+    console.log(silverGem);
+  var purpleGem = Math.floor(Math.random() * 12) + 1;
+    console.log(purpleGem);
 
+ //Create reset function
+ var reset = function () {
+  randomNumber = Math.floor(Math.random() * 120) + 19;
+  greenGem = Math.floor(Math.random() * 12) + 1;
+  multiGem = Math.floor(Math.random() * 12) + 1;
+  silverGem = Math.floor(Math.random() * 12) + 1;
+  purpleGem = Math.floor(Math.random() * 12) + 1;
+  totalScore = 0;
+ }  
+  
+    
+//Show random number
+  $("#random-number").text(randomNumber);
 
 
 
